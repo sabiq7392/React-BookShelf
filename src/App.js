55b69ab2ac53aae658858ebbcd9d8ca1c3f302d9
Sabiq Muhammad <sabi20153ti@student.nurulfikri.ac.id>
2021-10-02 22:44:21 +0700

    dev version deploy

---
+++ src/App.js
@@ -0,0 +1,16 @@
+// import logo from './assets/img/Logo.svg';
+import React from 'react';
+import './App.css';
+import Header from './assets/js/component.js';
+
+class App extends React.Component {
+  render() {
+    return (
+      <div className="App">
+        <Header />
+      </div>
+    );
+  }
+}
+
+export default App;
